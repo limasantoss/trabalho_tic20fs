ffadd69243a92c288a6f2f73247505de58dbbe83
Igor Santos <igorlimasantos667@gmail.com>
2025-06-10 00:09:03 -0300

    Update trabalhoFsm.js

--- igor/trabalhoFsm.js
+++ igor/trabalhoFsm.js
@@ -1,51 +1,35 @@
+// Função 1: Chegada dos amigos
 function chegadaDosAmigos() {
     let galera = ['👦', '👧'];
-    galera.push('🧑');
-    return galera;
+    galera.push('🧑');        
+    return galera;            
 }
 console.log(chegadaDosAmigos()); 
-// ['👦', '👧', '🧑']
-//  A galera foi chegando pro rolê
-
 function alguemVaiEmbora(galera) {
-    galera.pop();
-    return galera;
+    galera.pop();  
+    return galera;  
 }
 console.log(alguemVaiEmbora(['👦', '👧', '🧑']));
-// ['👦', '👧']
-//  Um amigo foi embora mais cedo.
-
-
 function prepararLanches() {
-    let lanches = ['🍞', '🧀', '🍅'];
-    let prontos = lanches.map(comida => comida + '🍔');
+    let lanches = ['🍞', '🧀', '🍅']; 
+    let prontos = lanches.map(comida => comida + '🍔'); 
     return prontos;
 }
 console.log(prepararLanches());
-// ['🍞🍔', '🧀🍔', '🍅🍔']
-// Cada ingrediente virou um hambúrguer.
-
 function quemFicouAcordado() {
-    let status = ['😴', '😎', '😴', '😎'];
+    let status = ['😴', '😎', '😴', '😎']; 
     let acordados = status.filter(pessoa => pessoa === '😎');
     return acordados;
 }
 console.log(quemFicouAcordado());
-// ['😎', '😎']
-//  Só os fortes ficaram acordados até tarde!
-
 function inverterBrincadeiras() {
-    let brincadeiras = ['⚽️', '🎲', '🎮'];
-    return brincadeiras.reverse();
+    let brincadeiras = ['⚽️', '🎲', '🎮']; 
+    return brincadeiras.reverse(); 
 }
 console.log(inverterBrincadeiras());
-// ['🎮', '🎲', '⚽️']
-// Decidiram brincar de videogame antes do futebol!
-
 function premiacao() {
-    let medalhas = ['🥉', '🥇', '🥈'];
-    return medalhas.sort();
+    let medalhas = ['🥉', '🥇', '🥈']; 
+    return medalhas.sort(); 
 }
 console.log(premiacao());
-// ['🥇', '🥈', '🥉']
-//  Ordenando os campeões!
+
